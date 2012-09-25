adb2e9c9ca608cbf08e8f92e10a92d77a0d17e1d
KallDrexx <me@mshapiro.net>
2012-09-24 23:24:04 -0400

    Added caching to the EumReflectionValueRetriever to help performance

--- GlueCsvEditor/KnownValues/EnumReflectionValueRetriever.cs
+++ GlueCsvEditor/KnownValues/EnumReflectionValueRetriever.cs
@@ -8,10 +8,32 @@ namespace GlueCsvEditor.KnownValues
 {
     public class EnumReflectionValueRetriever : IKnownValueRetriever
     {
+        protected static object _threadLock = new object();
+        protected static Dictionary<string, IEnumerable<string>> _cachedTypeValues;
+
         public IEnumerable<string> GetKnownValues(string fullTypeName)
         {
             if (string.IsNullOrWhiteSpace(fullTypeName))
                 return new string[0];
+
+            // Lock to prevent multiple threads from accessing the type dictionary at the same time
+            lock (_threadLock)
+            {
+                // If the dictionary hasn't been instantiated yet, set it up
+                if (_cachedTypeValues == null)
+                    _cachedTypeValues = new Dictionary<string, IEnumerable<string>>();
+
+                // Check if this type's value has already been cached
+                if (!_cachedTypeValues.ContainsKey(fullTypeName))
+                    CacheTypeValues(fullTypeName);
+
+                return _cachedTypeValues[fullTypeName];
+            }
+        }
+
+        protected void CacheTypeValues(string fullTypeName)
+        {
+            IEnumerable<string> foundValues;
 
             // Use reflection to retrieve the specified enum
             var type = AppDomain.CurrentDomain
@@ -22,12 +44,21 @@ namespace GlueCsvEditor.KnownValues
                                 .FirstOrDefault();
 
             if (type == null)
-                return new string[0];
+            {
+                foundValues = new string[0];
+            }
+            else
+            {
+                // Get all the enum values
+                foundValues = type.GetMembers(BindingFlags.Public | BindingFlags.Static)
+                                   .Select(x => x.Name)
+                                   .ToList();
+            }
 
-            // Get all the enum values
-            return type.GetMembers(BindingFlags.Public | BindingFlags.Static)
-                       .Select(x => x.Name)
-                       .ToList();
+            if (_cachedTypeValues.ContainsKey(fullTypeName))
+                _cachedTypeValues[fullTypeName] = foundValues;
+            else
+                _cachedTypeValues.Add(fullTypeName, foundValues);
         }
     }
 }
