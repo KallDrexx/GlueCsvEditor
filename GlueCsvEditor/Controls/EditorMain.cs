4d6076526e70df1b6502f171a73e6e04d38cd1e5
KallDrexx <me@mshapiro.net>
2012-10-05 23:35:56 -0400

    Added known values to the complex type property grid

--- GlueCsvEditor/Controls/EditorMain.cs
+++ GlueCsvEditor/Controls/EditorMain.cs
@@ -602,7 +602,7 @@ namespace GlueCsvEditor.Controls
 
             // If the complex type coudln't be parsed from the current value, create one manually
             if (complexType == null)
-                complexType = new ComplexTypeDetails { TypeName = type };
+                complexType = new ComplexTypeDetails { Namespace = ns, TypeName = type };
 
             // Go through all the properties and add any "known ones" that weren't part of the parsed set
             foreach (var prop in knownProperties)
@@ -620,7 +620,7 @@ namespace GlueCsvEditor.Controls
             }
 
             // Setup pgrid displayer
-            var displayer = new ComplexTypePropertyGridDisplayer();
+            var displayer = new ComplexTypePropertyGridDisplayer(_data);
             displayer.ComplexTypeUpdatedHandler = ComplexTypeUpdated;
             displayer.Instance = complexType;
             displayer.PropertyGrid = pgrPropertyEditor;
